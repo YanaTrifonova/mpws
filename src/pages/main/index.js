53c5cc08dcdbb25f1f682bd34faffe377bfc1e69
YanaTrifonova <trifonova.yana.prof@gmail.com>
2020-12-23 00:19:27 +0100

    added car rinding on the road

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,7 +1,16 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
+import car0 from "../../images/cars/car_0.png";
+import car1 from "../../images/cars/car_1.png";
+import car2 from "../../images/cars/car_2.png";
+import car3 from "../../images/cars/car_3.png";
+import car4 from "../../images/cars/car_4.png";
 import "./index.css"
 
 export default function Main() {
+    const [car, setCar] = useState(car0);
+    const [currentCar, setCurrentCar] = useState(0);
+    const carList = [car0, car1, car2, car3, car4];
+
     function handleRightArrowDown(e) {
         if (e.key === "ArrowRight") {
             const backgroundElementStyles = document.getElementById("background").classList;
@@ -26,6 +35,25 @@ export default function Main() {
         }
     }
 
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    function handleCarChange() {
+        console.log("💅🏼 💅🏿");
+        let anotherCar = false;
+
+        while (!anotherCar) {
+            let carIndex = getRandomInt(4);
+            if (carIndex !== currentCar) {
+                setCurrentCar(carIndex);
+                anotherCar = true;
+                setCar(carList[carIndex]);
+            }
+        }
+
+    }
+
     useEffect(() => {
         document.addEventListener("keydown", handleRightArrowDown, false);
         document.addEventListener("keyup", handleRightArrowUp, false);
@@ -37,6 +65,9 @@ export default function Main() {
     }, []);
 
     return (
-        <div id="background"/>
+        <>
+            <div id="background"/>
+            <img src={car} className="car" alt="car" onClick={handleCarChange}/>
+        </>
     )
 }
