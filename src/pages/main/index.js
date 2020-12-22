00c91cd1da1ac1afb3fa325d93143073121224fd
YanaTrifonova <trifonova.yana.prof@gmail.com>
2020-12-22 23:48:13 +0100

    added scrolling background for main page

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,8 +1,42 @@
-import "./index.css";
+import React, {useEffect} from "react";
+import "./index.css"
 
 export default function Main() {
+    function handleRightArrowDown(e) {
+        if (e.key === "ArrowRight") {
+            const backgroundElementStyles = document.getElementById("background").classList;
 
-    return(
-        <h1>Main Page</h1>
+            console.log("🚗 💨");
+            if (backgroundElementStyles.contains("pause")) {
+                backgroundElementStyles.remove("pause");
+            }
+
+            if (!backgroundElementStyles.contains("animation")) {
+                backgroundElementStyles.add("animation");
+            }
+        }
+    }
+
+    function handleRightArrowUp(e) {
+        const backgroundElementStyles = document.getElementById("background").classList;
+
+        if (e.key === "ArrowRight") {
+            console.log("🚘 ✋");
+            backgroundElementStyles.add("pause");
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener("keydown", handleRightArrowDown, false);
+        document.addEventListener("keyup", handleRightArrowUp, false);
+
+        return () => {
+            document.removeEventListener("keydown", handleRightArrowDown, false);
+            document.removeEventListener("keyup", handleRightArrowUp, false);
+        };
+    }, []);
+
+    return (
+        <div id="background"/>
     )
 }
